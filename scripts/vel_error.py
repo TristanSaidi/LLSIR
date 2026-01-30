5fca7f18975723ef169cb449b50ad1295f786792
Tristan Saidi <tsaidi@hydra1.stat.cmu.edu>
2026-01-30 12:57:10 +0000

    updating vel_error script

--- scripts/vel_error.py
+++ scripts/vel_error.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from src.curves import *
 from src.llsir import LLSIR
+from src.dwlls import DWLLS
 from src.link import *
 import argparse
 import os
@@ -9,9 +10,23 @@ import os
 
 curve_map = {
     'identity': identity,
-    'quadratic': quadratic,
+    'sin_curve': sin_curve,
     'circle': circle,
     'polygon': polygon
+}
+
+epsilon_map = {
+    'identity': 0.4,
+    'sin_curve': 0.2,
+    'circle': 0.1,
+    'polygon': 0.2
+}
+
+loc_map = {
+    'identity': np.array([0.7, 0.5]),
+    'sin_curve': np.array([0.6, 0.5]),
+    'circle': np.array([0.8, 0.5]),
+    'polygon': np.array([0.7, 0.5])
 }
 
 def main(args):
@@ -21,31 +36,41 @@ def main(args):
     curve = gamma(curve_fn)
 
     mean_sq_errors = []
-    epsilon_0 = 0.4
+    mean_sq_errors_dwlls = []
+    epsilon_0 = epsilon_map.get(args.curve)
 
-    Ns = np.arange(500, 10001, 500)
     trials = 50
+    # bake randomness into Ns
+    Ns = np.arange(500, 10001, 500)
+    
 
     # Fix randomness across N
     Nmax = Ns[-1]
-    X_full = np.random.rand(Nmax, 2)
+    X_full = [np.random.rand(Nmax, 2) for _ in range(trials)]
 
     # Fix x0 across N (optionally keep away from boundary)
-    x_0s = np.random.rand(trials, 2) * 0.8 + 0.1  # keep away from boundary
-
+    x_0 = np.array(loc_map.get(args.curve))
     for N in Ns:
         print(f"Running LLSIR with N = {N}")
-        X = X_full[:N]
         sq_errors = []
-        for x_0 in x_0s:
+        sq_error_dwlls = []
+        for X_full_i in X_full:
+            X = X_full_i[:N, :]
             epsilon = epsilon_0 * (N / Ns[0])**(-1 / (x_0.shape[0] + 4))  # d=2, but see note below
             # epsilon = epsilon_0
             llsir = LLSIR(X, curve, link_id, epsilon=epsilon, sigma=0.01)
             beta, y_hat, y_true = llsir.fit(x_0)
             true_beta = curve.unit_gradient(curve.project(x_0)[1])
+            if curve_fn is identity:
+                # for identity, compare to single index regression
+                dwlls = DWLLS(X, curve, link_id, sigma=0.01)
+                beta_dwlls = dwlls.fit_velocity()
+                sq_error_dwlls.append(min(np.linalg.norm(beta_dwlls - true_beta)**2, np.linalg.norm(beta_dwlls + true_beta)**2))
             sq_error = min(np.linalg.norm(beta - true_beta)**2, np.linalg.norm(beta + true_beta)**2)
             sq_errors.append(sq_error)
         mean_sq_errors.append(np.mean(sq_errors))
+        if curve_fn is identity:
+            mean_sq_errors_dwlls.append(np.mean(sq_error_dwlls))
         print(f"Mean squared error over {trials} trials: {np.mean(sq_errors)}")
 
     plt.rcParams.update({
@@ -55,8 +80,8 @@ def main(args):
     })
 
     plt.figure()
-    # plot log-log of sq_error vs N
-    plt.loglog(Ns, mean_sq_errors, marker='o', markersize=3)
+    # plot log-log of sq_error vs N -- make line of best fit the same color
+    plt.loglog(Ns, mean_sq_errors, marker='o', markersize=3, color='green')
     plt.xlim(Ns[0] * 0.9, Ns[-1] * 1.1)
     plt.xlabel(r'Number of samples $n$')
     plt.ylabel(r"$\|\hat{v}'(x_0) - \gamma'(\Pi(x_0))\|_2^2$")
@@ -72,6 +97,15 @@ def main(args):
     coeffs = np.polyfit(log_Ns, log_errors, 1)
     fitted_errors = np.exp(np.polyval(coeffs, log_Ns))
     plt.loglog(Ns, fitted_errors, linestyle=':', color='green', label=rf'Fitted slope, ${coeffs[0]:.2f}$')
+    if curve_fn is identity:
+        # fit line to dwlls data
+        log_errors_dwlls = np.log(mean_sq_errors_dwlls)
+        coeffs_dwlls = np.polyfit(log_Ns, log_errors_dwlls, 1)
+        fitted_errors_dwlls = np.exp(np.polyval(coeffs_dwlls, log_Ns))
+        # plot dwlls data and fitted line
+        plt.loglog(Ns, mean_sq_errors_dwlls, marker='s', markersize=3, color='orange')
+        plt.loglog(Ns, fitted_errors_dwlls, linestyle=':', color='orange', label=rf'DWLLS Fitted slope, ${coeffs_dwlls[0]:.2f}$')
+        
     plt.legend()
     dir = 'outputs'
     if not os.path.exists(dir):
